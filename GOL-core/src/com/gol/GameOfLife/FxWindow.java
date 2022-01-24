869d58e02848d96db494b6687555a6babf9631bc
Etienne <etiebr@gmail.com>
2022-01-24 09:23:00 +0100

    dud

--- GOL-core/src/com/gol/GameOfLife/FxWindow.java
+++ GOL-core/src/com/gol/GameOfLife/FxWindow.java
@@ -4,15 +4,19 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.awt.Toolkit;
 import java.awt.Dimension;
+import java.util.Arrays;
 import java.util.HashSet;
 
 public class FxWindow extends Application {
@@ -32,6 +36,10 @@ public class FxWindow extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         core = new GOLcore();
+        core.state[1][3] = true;
+        core.state[1][4] = true;
+        core.state[24][13] = true;
+        core.state[15][16] = true;
         stage.setTitle(title);
 
         //Universal ui elements
@@ -65,7 +73,39 @@ public class FxWindow extends Application {
         bottomUI.setAlignment(Pos.CENTER)   ;
         bottomUI.getChildren().addAll(save, succ, widthBox, heightBox, enter);
 
-        //todo Def main panel
+        //Def main panel
+        BorderPane mainBorder = new BorderPane();
+        Canvas canvas = new Canvas(core.size.width * core.tileSize.width, core.size.height * core.tileSize.height);
+        StackPane background = new StackPane(canvas);
+        background.setStyle("-fx-background-color: BLACK");
+        mainBorder.setCenter(background);
+        mainBorder.setBottom(bottomUI);
+        GraphicsContext graphics = canvas.getGraphicsContext2D();
+
+        graphics.setFill(Color.WHITE);
+        graphics.setStroke(Color.DARKGRAY);
+
+        if (showGrid) {
+            for (int i = 0; i < core.size.height; i++) {
+                graphics.strokeLine(0, i * core.tileSize.height, (int) canvas.getWidth(), i * core.tileSize.height);
+            }
+
+            for (int i = 0; i < core.size.width; i++) {
+                graphics.strokeLine(i * core.tileSize.width, 0, i * core.tileSize.width, (int) canvas.getHeight());
+            }
+        }
+
+        for (int i = 0; i < core.size.width; i++) {
+            for (int j = 0; j < core.size.height; j++) {
+                if (core.state[i][j]) {
+                    graphics.fillRect(
+                            (i * core.tileSize.width) + tileGap, j * (core.tileSize.height + tileGap),
+                            core.tileSize.width - tileGap, core.tileSize.height - tileGap);
+                }
+            }
+        }
+
+        Scene main = new Scene(mainBorder);
 
         //Def edit panel
         refreshEditTiles();
@@ -81,12 +121,13 @@ public class FxWindow extends Application {
 
         Scene edit = new Scene(editBorder);
         edit.getStylesheets().add(String.valueOf(this.getClass().getResource("/style.css")));
+
+        //Choose scene
         stage.setScene(edit);
         stage.show();
 
-
         //Button actions
-        enter.setOnAction(e-> {
+        enter.setOnAction(e -> {
             this.core.size.width = parseInt(widthField);
             this.core.size.height = parseInt(heightField);
 
@@ -102,9 +143,17 @@ public class FxWindow extends Application {
             heightField.setText(Integer.toString(this.core.size.height));
         });
 
-        /*save.setOnAction(e-> {
-            (x) -> core.state = editPaneTiles.getChildren().toArray()[((int) x)];
-        });*/
+        save.setOnAction(e -> {
+            for (int i = 0; i < core.size.height; i++) {
+                for (int j = 0; j < core.size.width; j++) {
+                    core.state[i][j] = ((CheckBox) editPaneTiles.getChildren().get(i * core.size.height + j)).isSelected();
+                }
+            }
+
+            //System.out.println(Arrays.deepToString(core.state)); //Debög
+
+            stage.setScene(main);
+        });
     }
 
     public int parseInt(TextField input){ //Convenience bundle; checks and returns integer from a text field
